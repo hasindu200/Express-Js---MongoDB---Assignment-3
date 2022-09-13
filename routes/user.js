ef8e82baa81d91eacda3bc49892bee0372b7942f
hasindu200 <hasindukl@gmail.com>
2022-09-13 23:50:10 +0530

    complet the user routes

--- routes/user.js
+++ routes/user.js
@@ -13,5 +13,62 @@ userRout.post('/',async (req,res)=>{
         phoneNumber: req.body.phoneNumber,
         email: req.body.email
     })
+    try {
+        const response = await user.save()
+        res.json(response)
+    }catch (err) {
+        res.send('Err:' + err)
+    }
+})
+
+userRout.get('/',async (req,res) =>{
+    try {
+        const users = await User.find()
+        res.send(users)
+    }catch (err) {
+        res.send('Err:'+ err)
+    }
+})
+
+userRout.get('/:id',async (req,res) =>{
+    try {
+        const user = await User.findById(req.params.id)
+        res.json(user)
+    }catch (err) {
+    }
+})
+
+userRout.delete('/:id',async(req,res)=> {
+
+    try {
+        const user = await User.findById(req.params.id)
+        const response = await user.remove()
+        res.json(response)
+    } catch (err) {
+        res.send('Err:' + err)
+    }
 
 })
+
+
+userRout.put('/:id',async(req,res)=>{
+    try {
+        const user = await User.findById(req.params.id)
+        user.firstName = req.body.firstName,
+            user.surName = req.body.surName,
+            user.gender = req.body.gender,
+            user.dateOfBirth = req.body.dateOfBirth,
+            user.password = req.body.password,
+            user.phoneNumber = req.body.phoneNumber,
+            user.email = req.body.email
+
+        const response = await user.save()
+        res.json(response)
+
+    } catch (err) {
+        res.send('Err: ' + err)
+    }
+})
+
+
+module.exports = userRout
